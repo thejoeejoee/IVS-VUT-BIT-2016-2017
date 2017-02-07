4824efc49639f5e8cfd28e3cded8cba3bee47afe
Josef Kolář <xkolar71@stud.fit.vutbr.cz>
2017-02-07 23:15:37 +0100

    Change of division every test case

--- src/tests/calculator/solver/test_solver.py
+++ src/tests/calculator/solver/test_solver.py
@@ -8,44 +8,56 @@ class TestSolver(TestCase):
     def setUp(self):
         self.solver = Solver()
 
-    def test_compute_basic_expressions(self):
+    def test_compute_basic_expression_add(self):
         self.assertEqual(
             self.solver.compute('40 + 2'),
             42,
             'The answer to life the universe and everything (and numbers adding).'
         )
+
+    def test_compute_basic_expression_subtract(self):
         self.assertEqual(
             self.solver.compute('40 - 2'),
             38,
             'Subtracting two positive numbers.'
         )
+
+    def test_compute_basic_expression_multiply(self):
         self.assertEqual(
             self.solver.compute('2 * 8'),
             16,
             'Multiplying two positive numbers.'
         )
+
+    def test_compute_basic_expression_divide(self):
         self.assertEqual(
             self.solver.compute('9 / 3'),
             3,
             'Integer division two positive non-zero numbers.'
         )
 
-    def test_compute_bracketed_expressions(self):
+    def test_compute_bracketed_expression_add(self):
         self.assertEqual(
             self.solver.compute('40 + (2 + 0)'),
             42,
             'Adding with inner expression.'
         )
+
+    def test_compute_bracketed_expression_subtract(self):
         self.assertEqual(
             self.solver.compute('40 - (2 + 6)'),
             32,
             'Subtracting of inner expression in brackets.'
         )
+
+    def test_compute_bracketed_expression_multiply(self):
         self.assertEqual(
             self.solver.compute('2 * (8 * 2)'),
             32,
             'Multiplying with inner expression.'
         )
+
+    def test_compute_bracketed_expression_divide(self):
         self.assertEqual(
             self.solver.compute('(9 / 3) / 3'),
             1,
