64d9af7d9a2b9a567b3cb74570d73097ea74ecc2
Sony <sirarseus@gmail.com>
2017-02-07 22:02:56 +0100

    Init application base

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 # coding=utf-8
 # TODO main script for start of calculator
+
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    app  = QApplication(sys.argv)
+
+    sys.exit(app.exec())
