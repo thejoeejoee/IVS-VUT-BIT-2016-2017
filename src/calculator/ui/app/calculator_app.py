6dba415d008641d52ab7e5c1498cae553c921ded
Sony <sirarseus@gmail.com>
2017-02-08 15:25:26 +0100

    Fix add coding

--- src/calculator/ui/app/calculator_app.py
+++ src/calculator/ui/app/calculator_app.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from typing import List
 
 from PyQt5.QtGui import QGuiApplication
--- src/calculator/ui/mainwindow/__init__.py
+++ src/calculator/ui/mainwindow/__init__.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from calculator.ui.mainwindow.calculator_window import CalculatorWindow
 
 __all__ = ("CalculatorWindow", )
