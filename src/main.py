040ac76507db34e627ac11c930ca369b775eea3f
Sony <sirarseus@gmail.com>
2017-02-08 18:15:55 +0100

    Expose exception

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@ import sys
 
 from calculator.ui.app import CalculatorApp
 
+sys._excepthook = sys.excepthook
+def exception_hook(exctype, value, traceback):
+    sys._excepthook(exctype, value, traceback)
+sys.excepthook = exception_hook
 
 if __name__ == "__main__":
     app = CalculatorApp(sys.argv)
