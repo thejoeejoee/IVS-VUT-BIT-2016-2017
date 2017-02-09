6085e8cd66c38506fb61c87190ee16cd2e46bfae
Josef Kolář <xkolar71@stud.fit.vutbr.cz>
2017-02-09 11:39:03 +0100

    Add test runner fallback for systems without installed curses

--- src/test.py
+++ src/test.py
@@ -3,7 +3,13 @@
 import unittest
 from os.path import dirname
 
-from colour_runner.runner import ColourTextTestRunner
+try:
+    from colour_runner.runner import ColourTextTestRunner as TestRunner
+    # ColourTextTestRunner needs installed curses, which may be problem on Windows based systems
+except ImportError:
+    # fallback to default text test runner
+    from unittest import TextTestRunner as TestRunner
+
 
 from tests import calculator
 
@@ -12,7 +18,7 @@ def main():
     loader = unittest.TestLoader()
 
     suite = loader.discover(dirname(calculator.__file__), pattern='*.py')
-    runner = ColourTextTestRunner(verbosity=10)
+    runner = TestRunner(verbosity=10)
     result = runner.run(unittest.TestSuite(suite))
 
     exit(len(result.errors + result.failures))
