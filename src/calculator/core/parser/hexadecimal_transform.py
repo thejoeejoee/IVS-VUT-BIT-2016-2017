af6bb908f9d940e064cb33a49cbf458baffb008c
Josef Kolář <thejoeejoee@gmail.com>
2017-02-12 00:08:46 +0100

    Fix doc comment for hexadecimal transform

--- src/calculator/core/parser/hexadecimal_transform.py
+++ src/calculator/core/parser/hexadecimal_transform.py
@@ -8,20 +8,14 @@ from typing import Optional
 class HexadecimalTransform(NodeTransformer):
     """
     Transforms all hexadecimal convertible Name nodes to directly Num node value.
-
     """
 
     HEXADECIMAL_REGEX = re.compile('^(0x|0X)?[A-F0-9]+$')
 
     def visit_Name(self, node: expr) -> Optional[expr]:
         """
-        Subscript(
-            value=Name(id='data', ctx=Load()),
-            slice=Index(value=Str(s=node.id)),
-            ctx=node.ctx
-        )
-        :param node:
-        :return:
+        :param node: Name node of found variable in processed AST
+        :return: Num node, if variable id can be converted to Num
         """
         if self.HEXADECIMAL_REGEX.fullmatch(node.id):
             return Num(n=int(x=node.id, base=16))
