428ea2278bb8b0f38936acbdbcc19f1233b572f8
Josef Kolář <thejoeejoee@gmail.com>
2017-02-12 00:51:15 +0100

    Support for hexadecimals literals in expressions (#9)
    * Add parser transforms support with first transform class
    
    * Add type hints to parser
    
    * Add test for wanted functionality of hexadecimal transform
    
    * Add regex to HexadecimalTransform to resolve correct hexadecimals names
    
    * Add test for not modified Name node after usage of HexadecimalTransform
    
    * Fix doc comment for hexadecimal transform

--- src/calculator/core/parser/parser.py
+++ src/calculator/core/parser/parser.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 import ast
 from _ast import AST
+from typing import Iterable, Sized, Optional
+
+from calculator.core.parser.hexadecimal_transform import HexadecimalTransform
 
 
 class Parser(object):
@@ -10,9 +13,55 @@ class Parser(object):
 
     """
 
+    # TODO solve problem with hexadecimals literals like '4A'
+    DEFAULT_TRANSFORMS = (
+        HexadecimalTransform,
+    )
+
+    def __init__(self, transforms: Optional[Iterable] = ()):
+        super().__init__()
+
+        self._transforms = ()
+        if transforms is None:
+            self.transforms = self.DEFAULT_TRANSFORMS
+        else:
+            self.transforms = transforms
+
     def parse(self, expression: str) -> AST:
         tree = ast.parse(
             source=expression,
             mode='eval'
         )
+        for transform in self._transforms:
+            tree = transform(tree)
+
         return tree
+
+    def set_transforms(self, transforms: Sized):
+        """
+        Sets node transforms for process AST before returning.
+        New value could be iterable of:
+        1. NodeTransformer subclasses, will be instantiated and used with .visit method
+        2. NodeTransformer instances, will by used the .visit method
+        3. callable object, directly the item will be used
+        4. otherwise AssertionError raised
+        :param transforms: Iterable of items described above
+        """
+        assert isinstance(transforms, Iterable)
+        self._transforms = tuple(filter(
+            None,
+            ((
+                 transform().visit  # given as class, so instantiated and get the .visit
+                 if isinstance(transform, type) and issubclass(transform, ast.NodeTransformer)
+                 else transform.visit  # given and transform object, directly get the .visit
+                 if isinstance(transform, ast.NodeTransformer)
+                 else transform  # directly callable
+                 if callable(transform)
+                 else None  # otherwise filtered by filter(None, ...)
+             ) for transform in transforms
+             )
+        ))
+        # TODO: Assertion or TypeError?
+        assert len(self._transforms) == len(transforms), 'Unknown transform(s) given.'
+
+    transforms = property(fset=set_transforms)
---
+++ src/tests/calculator/core/parser/test_hexadecimal_transform.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+from _ast import Name, Num
+from unittest import TestCase
+
+from calculator.core.parser.hexadecimal_transform import HexadecimalTransform
+
+
+class TestHexadecimalTransform(TestCase):
+    def setUp(self):
+        self.transform = HexadecimalTransform()
+
+    def test_simple_hexadecimal_number(self):
+        name = Name(id='A1')
+        num = self.transform.visit(name)
+        self.assertIsInstance(
+            num,
+            Num,
+            'Transformed hexadecimal literal as Name to Num object.'
+        )
+        self.assertEqual(
+            num.n,
+            int('A1', base=16)
+        )
+
+    def test_not_modified_variable_name(self):
+        name = Name(id='a1')
+        transformed_name = self.transform.visit(name)
+        self.assertIs(
+            transformed_name,
+            name,
+            'Node variable "a1" node should stay as Name node.'
+        )
