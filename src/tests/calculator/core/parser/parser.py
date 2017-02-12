0c1ea252dda77336055638952cc981fec987cf18
Josef Kolář <thejoeejoee@gmail.com>
2017-02-12 01:34:34 +0100

    Add test for invalid inputs for parser

--- src/tests/calculator/core/parser/parser.py
+++ src/tests/calculator/core/parser/parser.py
@@ -9,8 +9,25 @@ class ParserTest(TestCase):
     def setUp(self):
         self.parser = Parser()
 
-        # TODO test for parser, asserts for valid/invalid inputs (by syntax, no semantic)
+    # TODO test for parser, asserts for valid/invalid inputs (by syntax, no semantic)
 
-    def test_invalid_number_literal(self):
-        with self.assertRaises(ParserSyntaxError):
+    def test_letter_at_end_of_number(self):
+        with self.assertRaises(ParserSyntaxError, msg='Invalid number literal with letter at end.'):
             self.parser.parse('42x')
+
+    def test_trailing_brackets(self):
+        with self.assertRaises(ParserSyntaxError, msg='Invalid expression with trailing bracket at end.'):
+            self.parser.parse('1 + 8)')
+
+    def test_missed_next_token(self):
+        with self.assertRaises(ParserSyntaxError,
+                               msg='Invalid expression with missing next token for binary operation.'):
+            self.parser.parse('1 +')
+
+    def test_standalone_operator(self):
+        with self.assertRaises(ParserSyntaxError, msg='Invalid expression with binary operation without operands.'):
+            self.parser.parse('+')
+
+    def test_duplicated_dot_in_number_literal(self):
+        with self.assertRaises(ParserSyntaxError, msg='Invalid number with doubled dot in number.'):
+            self.parser.parse('0..1')
