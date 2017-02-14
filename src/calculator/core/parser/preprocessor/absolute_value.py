bf41db3489162402501b302754613f2b22d5cc13
Meigurad <robert.navratil96@seznam.cz>
2017-02-14 23:03:24 +0100

    Improvements for AbsoluteValuePreprocessor

--- src/calculator/core/parser/preprocessor/absolute_value.py
+++ src/calculator/core/parser/preprocessor/absolute_value.py
@@ -14,7 +14,7 @@ class AbsoluteValuePreprocessor(object):
         if self.ABSOLUTE_VALUE_SIGN not in expression:
             return expression
 
-        expression = abs(expression)
+        expression = self.abs(expression)
         return expression
     def abs(self, string):
 
@@ -25,73 +25,76 @@ class AbsoluteValuePreprocessor(object):
         cisla = "0123456789"
 
         """ Osetreni zda neni lichy pocet zavorek """
-        if (pocet % 2):
+        if(pocet%2):
             return "ERROR"
         else:
             """ Zakladni promenne pro praci """
-            counter = 0  # pocet pruchodu cyklem for
-            abs_vystup = ''  # vysledny retezec
-            pocet_svislitka = 0  # pocet '|' znaku
-            je_cislo = 0  # prakticky bool => nabyva hodnoty 1/0 viz cyklus while
-            je_operator = 0  # stejne jako 'pocet_operatory'
+            counter = 0             #pocet pruchodu cyklem for
+            abs_vystup = ''         #vysledny retezec
+            pocet_svislitka = 0     #pocet '|' znaku
+            je_cislo = 0            #prakticky bool => nabyva hodnoty 1/0 viz cyklus while
+            je_operator_za = 0         #stejne jako 'pocet_operatory'
+            je_operator_pred = 0
 
             for znak in string:
                 """ Pomocne indexovaci hodnoty pro 'lepsi' orientaci """
-                index_1 = counter - 1
-                index1 = counter + 1
-                index2 = counter + 2
+                index_1 = counter-1
+                index1 = counter+1
+                index2 = counter+2
                 index = counter
 
                 """ Aby se osetril zasah mimo pole jsou indexy od urcite pozice stejne jako 'counter' """
-                if (counter == (len(string) - 1)):
+                if(counter == (len(string)-1)):
                     index1 = counter
-                if (counter == (len(string) - 2)):
+                if(counter == (len(string)-2)):
                     index2 = counter
 
                 """ Na zaklade prochazeni vstupniho retezce po nejblizsi '|' se urci zda je to zacatek a nebo konec absolutni hodnoty """
-                while (((string[index] != "|")) and (index != (len(string) - 1))):
-                    if (string[index] in cisla):
+                while(index < (len(string)-1) and string[index] != "|"):
+                    if((index == counter) and (string[index] in operatory)):
+                        je_operator_pred = 1
+                    elif (string[index] in cisla):
                         je_cislo = 1
                     elif (string[index] in operatory):
-                        je_operator = 1
+                        je_operator_za = 1
                     index += 1
 
-                if (znak == '|'):
+                if(znak == '|'):
                     """ Asi to jde zjednodusit ale ted na to nemam :D """
                     pocet_svislitka += 1
-                    if (pocet_svislitka > (pocet / 2)):
+                    if(pocet_svislitka > (pocet / 2)):
                         if ((string[index_1]) in operatory):
-                            if ((je_operator and je_cislo) or je_operator):
-                                abs_vystup += 'abs('
-                            else:
+                            if (je_operator_pred and je_cislo and not je_operator_za):
                                 abs_vystup += ')'
+                            else:
+                                abs_vystup += 'abs('
                         else:
                             abs_vystup += ')'
                     else:
                         if (string[index1] in operatory):
                             if (string[index2] in ("|" + cisla + promenne)):
-                                if ((je_operator and je_cislo) or (je_operator) or (counter == 0)):
-                                    abs_vystup += 'abs('
-                                else:
+                                if(je_operator_pred and je_cislo and not je_operator_za and (counter > 0)):
                                     abs_vystup += ')'
+                                else:
+                                    abs_vystup += 'abs('
                             else:
                                 abs_vystup += ')'
                         elif (string[index1] not in (cisla + promenne + "+-|")):
                             return "ERROR"
                         else:
                             abs_vystup += 'abs('
-                    je_operator = je_cislo = 0
-                elif (znak in operatory):
+                    je_operator_pred = je_operator_za = je_cislo = 0
+                elif(znak in operatory):
                     abs_vystup += znak
-                elif (znak in promenne):
+                elif(znak in promenne):
                     abs_vystup += znak
-                elif (znak in cisla):
+                elif(znak in cisla):
                     abs_vystup += znak
-                    je_operator = 0
+                    je_operator_za = 0
                 else:
                     return "ERROR"
                 counter += 1
-            if (abs_vystup.count('(') == abs_vystup.count(')')):
-                print(abs_vystup)
+            if(abs_vystup.count('(') == abs_vystup.count(')')):
+                return abs_vystup
             else:
                 return "ERROR"
