f78ff1b302c7d20766fc2b027095590a43b13ec6
Josef Kolář <xkolar71@stud.fit.vutbr.cz>
2017-02-15 22:36:37 +0100

    Add implementation with test for unary operation resolving

--- src/calculator/core/solver/solver.py
+++ src/calculator/core/solver/solver.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from ast import BinOp, Add, Num, Sub, Div, Mult, Call, AST
+from ast import BinOp, Add, Num, Sub, Div, Mult, Call, AST, UnaryOp, USub
 from typing import Union
 
 from calculator.core.math.math import Math
@@ -62,14 +62,19 @@ class Solver(object):
 
         return operation(self._resolve(left), self._resolve(right))
 
-    @_resolve.register(Num)
-    def _(self, num: Num) -> Union[int, float]:
+    @_resolve.register(UnaryOp)
+    def _(self, unary_op: UnaryOp) -> Union[int, float]:
         """
-        Returns resolved numeric value.
-        :param num: Num Node
-        :return: standard python number values
+        Endpoint for unary operations.
+        :param unary_op: UnaryOp instance (operation and operand)
+        :return: result
         """
-        return num.n
+        op, operand = unary_op.op, unary_op.operand
+
+        if isinstance(op, USub):
+            return - self._resolve(operand)
+        else:
+            return + self._resolve(operand)
 
     @_resolve.register(Call)
     def _(self, call: Call) -> Union[int, float]:
@@ -78,8 +83,18 @@ class Solver(object):
         :param call: Call node
         :return: result of the called function
         """
+        # TODO I am not sure, if call.func is always Name node with .id attribute
         function = self.function_calls_table.get(call.func.id)
         if callable(function):
             return function(*map(self._resolve, call.args))
         else:
             raise NotImplementedError(call.func.id)
+
+    @_resolve.register(Num)
+    def _(self, num: Num) -> Union[int, float]:
+        """
+        Returns resolved numeric value.
+        :param num: Num Node
+        :return: standard python number values
+        """
+        return num.n
--- src/tests/calculator/core/solver/solver_resolve.py
+++ src/tests/calculator/core/solver/solver_resolve.py
@@ -1,5 +1,7 @@
 # coding=utf-8
-from ast import Num, BinOp, AST
+from ast import Num, BinOp, AST, UnaryOp, USub, UAdd, Name, Call
+from operator import attrgetter
+from random import randint
 from unittest import TestCase
 
 from calculator.core.solver import Solver
@@ -50,3 +52,48 @@ class SolverResolveTest(TestCase):
 
         with self.assertRaises(NotImplementedError, msg='Not implemented error for unknown ast node.'):
             self.solver._resolve(UnknownNode())
+
+    def test_unary_operation(self):
+        self.assertEqual(
+            -42,
+            self.solver._resolve(UnaryOp(op=USub(), operand=Num(42))),
+            'Unary subtract should return negated number value.'
+        )
+        self.assertEqual(
+            42,
+            self.solver._resolve(UnaryOp(op=UAdd(), operand=Num(42))),
+            'Unary subtract should return negated number value.'
+        )
+
+    def test_call_node(self):
+        called = False
+        called_args = ()
+
+        def function(*args):
+            nonlocal called, called_args
+            called = True
+            called_args = args
+            return id(function)
+
+        self.solver.function_calls_table = {
+            function.__name__: function
+        }
+        args_to_call = tuple(Num(n=randint(5, 50)) for _ in range(randint(1, 10)))
+        self.assertEqual(
+            id(function),
+            self.solver._resolve(
+                Call(
+                    func=Name(
+                        id=function.__name__,
+                    ),
+                    args=args_to_call
+                )
+            ),
+            'Resolve Call should return same value as mocked function.'
+        )
+        self.assertTupleEqual(
+            tuple(map(attrgetter('n'), args_to_call)),
+            called_args,
+            'Args given to mocked function should be same as given to _resolve.'
+        )
+        self.assertTrue(called, 'Function should to be called.')
