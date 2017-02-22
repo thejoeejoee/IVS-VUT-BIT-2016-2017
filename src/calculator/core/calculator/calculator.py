4283dc73336cb9d2bbf80d9770212300a47370de
Martin Omacht <enkeymc@gmail.com>
2017-02-22 23:21:01 +0100

    Simple variable assign

--- src/calculator/core/calculator/calculator.py
+++ src/calculator/core/calculator/calculator.py
@@ -1,10 +1,11 @@
 # coding=utf-8
-from ast import Assign
+from ast import Assign, Name
 from typing import Dict, Tuple
 
 from calculator.core.solver.solver import Solver
 from calculator.typing import NumericValue, Variable
 from calculator.utils import OrderedDefaultDict
+from calculator.exceptions import VariableError
 
 
 class Calculator(object):
@@ -34,8 +35,13 @@ class Calculator(object):
         """
         root_node = self._solver.parser.parse(expression=expression)
         if isinstance(root_node, Assign):
-            pass  # TODO: resolve assign and create new variable from source expression
+            # TODO: resolve assign and create new variable from source expression
             # TODO: syntax restrict for assign like a, b = 1, 2
+            if len(root_node.targets) is not 1 or not isinstance(root_node.targets[0], Name):
+                raise VariableError()
+
+            value = self._solver.compute(root_node.value)
+            self._variables[root_node.targets[0].id] = value
         else:
             pass  # TODO from known variables resolve via self.solver result of given expression and set to Ans
 
