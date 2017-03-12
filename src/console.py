caad2f1c2f38f26b9990c8e31171fb1b27da85c2
Josef Kolář <xkolar71@stud.fit.vutbr.cz>
2017-03-12 14:36:10 +0100

    Improve of console calculator

--- src/console.py
+++ src/console.py
@@ -1,6 +1,21 @@
+from pprint import pformat
+
 from calculator.core.calculator import Calculator
 
 calculator = Calculator()
 
+try:
+	import readline
+except ImportError:
+	pass
+
 while True:
-    print(calculator.process(input()))
+	user_input = input(' >>> ').strip()
+	if not user_input:
+		continue
+	try:
+		result, variables = calculator.process(user_input)
+		print(" === {}\n === {}\n".format(result, pformat(dict(variables))))
+	except Exception as e:
+		print(e)
+	
