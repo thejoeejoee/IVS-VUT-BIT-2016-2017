e6b84f8ee2b830dac97b56e9d41c22cba4b41bbf
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2017-03-22 14:10:14 +0100

    Update translations

--- src/calculator/translations/cs.ts
+++ src/calculator/translations/cs.ts
@@ -4,22 +4,27 @@
 <context>
     <name>Adapter</name>
     <message>
-        <location filename="../ui/adapter.py" line="113"/>
+        <location filename="../ui/adapter.py" line="37"/>
+        <source>Unsupported base.</source>
+        <translation>Nepodporovaná číselná soustava.</translation>
+    </message>
+    <message>
+        <location filename="../ui/adapter.py" line="72"/>
         <source>Math error occured.</source>
         <translation>Matematická chyba.</translation>
     </message>
     <message>
-        <location filename="../ui/adapter.py" line="115"/>
+        <location filename="../ui/adapter.py" line="74"/>
         <source>Error in defining variable.</source>
         <translation>Chyba při definování proměnné.</translation>
     </message>
     <message>
-        <location filename="../ui/adapter.py" line="117"/>
+        <location filename="../ui/adapter.py" line="76"/>
         <source>Result is too big.</source>
         <translation>Výsledek je příliš velký.</translation>
     </message>
     <message>
-        <location filename="../ui/adapter.py" line="111"/>
+        <location filename="../ui/adapter.py" line="70"/>
         <source>Expression contains syntax error.</source>
         <translation>Výraz obsahuje syntaktickou chybu.</translation>
     </message>
@@ -27,20 +32,18 @@
 <context>
     <name>Error</name>
     <message>
-        <location filename="../ui/qml/visualization/Error.qml" line="63"/>
         <source>Error</source>
-        <translation>Chyba</translation>
+        <translation type="vanished">Chyba</translation>
     </message>
     <message>
-        <location filename="../ui/qml/visualization/Error.qml" line="88"/>
         <source>Ok</source>
-        <translation>Ok</translation>
+        <translation type="vanished">Ok</translation>
     </message>
 </context>
 <context>
     <name>ExpressionInput</name>
     <message>
-        <location filename="../ui/qml/controls/ExpressionInput.qml" line="26"/>
+        <location filename="../ui/qml/controls/ExpressionInput.qml" line="27"/>
         <source>Enter expression...</source>
         <translation>Zadejte výraz...</translation>
     </message>
@@ -52,13 +55,41 @@
         <source>Easter egg</source>
         <translation>Easter egg</translation>
     </message>
+    <message>
+        <location filename="../ui/easteregg/Game.qml" line="149"/>
+        <source>You are looser.</source>
+        <translation>Jste looser.</translation>
+    </message>
+    <message>
+        <location filename="../ui/easteregg/Game.qml" line="151"/>
+        <source>AI is noob.</source>
+        <translation>AI je noob. </translation>
+    </message>
+</context>
+<context>
+    <name>PopUp</name>
+    <message>
+        <location filename="../ui/qml/visualization/PopUp.qml" line="87"/>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
 </context>
 <context>
     <name>main</name>
     <message>
-        <location filename="../ui/qml/main.qml" line="24"/>
+        <location filename="../ui/qml/main.qml" line="27"/>
         <source>Barbie Calculator</source>
         <translation>Barbie Calculator</translation>
+    </message>
+    <message>
+        <location filename="../ui/qml/main.qml" line="193"/>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <location filename="../ui/qml/main.qml" line="207"/>
+        <source>Info</source>
+        <translation>Info</translation>
     </message>
 </context>
 </TS>
