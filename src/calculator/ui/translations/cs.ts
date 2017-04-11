d23646166219e9d58fe1ef6c6da4d598e0b08de8
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2017-04-12 01:24:56 +0200

    Update translations

--- src/calculator/ui/translations/cs.ts
+++ src/calculator/ui/translations/cs.ts
@@ -2,9 +2,32 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="cs_CZ" sourcelanguage="en_GB">
 <context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/windows/About.qml" line="86"/>
+        <source>Barbie calculator</source>
+        <translation>Barbie calculator</translation>
+    </message>
+    <message>
+        <location filename="../qml/windows/About.qml" line="104"/>
+        <source>From revision</source>
+        <translation>Z revize</translation>
+    </message>
+    <message>
+        <location filename="../qml/windows/About.qml" line="105"/>
+        <source>Created by:</source>
+        <translation>Vytvořeno:</translation>
+    </message>
+    <message>
+        <location filename="../qml/windows/About.qml" line="111"/>
+        <source>All rights reserved.</source>
+        <translation>Všechna práva vyhrazena.</translation>
+    </message>
+</context>
+<context>
     <name>Adapter</name>
     <message>
-        <location filename="../adapter.py" line="34"/>
+        <location filename="../adapter.py" line="35"/>
         <source>Unsupported base.</source>
         <translation>Nepodporovaná číselná soustava.</translation>
     </message>
@@ -13,41 +36,46 @@
         <translation type="vanished">Matematická chyba.</translation>
     </message>
     <message>
-        <location filename="../adapter.py" line="71"/>
+        <location filename="../adapter.py" line="72"/>
         <source>Math error occurred.</source>
         <translation>Matematická chyba.</translation>
     </message>
     <message>
-        <location filename="../adapter.py" line="73"/>
+        <location filename="../adapter.py" line="74"/>
         <source>Error in defining variable.</source>
         <translation>Chyba při definování proměnné.</translation>
     </message>
     <message>
-        <location filename="../adapter.py" line="75"/>
+        <location filename="../adapter.py" line="76"/>
         <source>Result is too big.</source>
         <translation>Výsledek je příliš velký.</translation>
     </message>
     <message>
-        <location filename="../adapter.py" line="77"/>
+        <location filename="../adapter.py" line="78"/>
         <source>Given parameters does not match to function {}.</source>
         <translation>Předané parametry neodpovídají funkci {}.</translation>
     </message>
     <message>
-        <location filename="../adapter.py" line="83"/>
+        <location filename="../adapter.py" line="84"/>
         <source>This construct is not supported.</source>
         <translation>Tato konstrukce není podporovaná.</translation>
+    </message>
+    <message>
+        <location filename="../adapter.py" line="121"/>
+        <source>#TODO</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Parameters count does not match function.</source>
         <translation type="vanished">Počet parametrů neodpovídá funkci.</translation>
     </message>
     <message>
-        <location filename="../adapter.py" line="81"/>
+        <location filename="../adapter.py" line="82"/>
         <source>Function is not defined.</source>
         <translation>Funkce není definována.</translation>
     </message>
     <message>
-        <location filename="../adapter.py" line="69"/>
+        <location filename="../adapter.py" line="70"/>
         <source>Expression contains syntax error.</source>
         <translation>Výraz obsahuje syntaktickou chybu.</translation>
     </message>
@@ -90,6 +118,14 @@
     </message>
 </context>
 <context>
+    <name>Help</name>
+    <message>
+        <location filename="../qml/windows/Help.qml" line="22"/>
+        <source>Help</source>
+        <translation>Nápověda</translation>
+    </message>
+</context>
+<context>
     <name>PopUp</name>
     <message>
         <location filename="../qml/visualization/PopUp.qml" line="112"/>
@@ -98,19 +134,93 @@
     </message>
 </context>
 <context>
-    <name>main</name>
+    <name>help</name>
     <message>
-        <location filename="../qml/main.qml" line="28"/>
         <source>Barbie Calculator</source>
-        <translation>Barbie Calculator</translation>
+        <translation type="obsolete">Barbie Calculator</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="208"/>
+        <location filename="../assets/contents/help.js" line="4"/>
+        <source>Manual on how to use Barbie Calculator.</source>
+        <translation>Manuál jak používat Barbie Calculator.</translation>
+    </message>
+    <message>
+        <location filename="../assets/contents/help.js" line="7"/>
+        <source>Getting started</source>
+        <translation>Začínáme</translation>
+    </message>
+    <message>
+        <location filename="../assets/contents/help.js" line="3"/>
+        <source>Barbie calculator</source>
+        <translation>Barbie calculator</translation>
+    </message>
+    <message>
+        <location filename="../assets/contents/help.js" line="8"/>
+        <source>First we need to enter expression, which we want to calculate into text input. Then we press the button with equal sign or press Enter/Return key. Congratulations you have done your very first calculation using Barbie calculator.</source>
+        <translation>Nejdříve musíme zadat výraz, který chceme vypočítat, do textového vstupu. Následně stiskneme tlačítko se symbolem rovnítka nebo zmáčkneme klávesu Enter. Gratulujeme provedli jste svůj první výpočet pomocí Barbie calculatoru.</translation>
+    </message>
+    <message>
+        <location filename="../assets/contents/help.js" line="11"/>
+        <source>Advance</source>
+        <translation>Pokročilé</translation>
+    </message>
+    <message>
+        <location filename="../assets/contents/help.js" line="14"/>
+        <source>Using completer</source>
+        <translation>Použití našeptávače</translation>
+    </message>
+    <message>
+        <location filename="../assets/contents/help.js" line="15"/>
+        <source>To show completer press Ctrl+Space. Completer will be shown automatically every time after entering operator. To choose item you want to complete click on the item or use arrows to navigate to item and then pres key Enter/Return.</source>
+        <translation>K zobrazení našeptávače stiskněte Ctrl+Mezerník. Našeptávač se zobrazí automaticky pokaždém zadání operátoru. Můžeme vybrat položku kliknutím nebo posunout se pomocí šipek a poté stisknout klávesu Enter.</translation>
+    </message>
+    <message>
+        <location filename="../assets/contents/help.js" line="18"/>
+        <source>Variables intro</source>
+        <translation>Začínáme s proměnnými</translation>
+    </message>
+    <message>
+        <location filename="../assets/contents/help.js" line="19"/>
+        <source>Barbie calculator also provide support of custom variables. To create variable &apos;a&apos; simply enter assignment of &apos;a&apos; as &apos;a=&apos; and then you add expression. If variable depends on another variable then it&apos;s value will be updated every time any of the dependent variables change.</source>
+        <translation>Barbie calculator také podporuje tvorbu vlastních proměnných. Abychom vytvořili proměnnou &apos;a&apos;, jednoduše zadáme přiřazení &apos;a&apos; tedy &apos;a=&apos; a poté doplníme výraz. Pokud proměnná závisí na jiných proměnných, pak bude její hodnota přepočítá, ppokaždé když hodnota proměnných, na kterých je závislá, změní. </translation>
+    </message>
+    <message>
+        <location filename="../assets/contents/help.js" line="22"/>
+        <source>Variables manipulation</source>
+        <translation>Manipulace s proměnnými</translation>
+    </message>
+    <message>
+        <location filename="../assets/contents/help.js" line="23"/>
+        <source>To enter into input expression from which variable was calculated click on the expression of variable in variable panel. If you click on variable in panel it will enter it&apos;s identifier and after right-click it&apos;s value. To delete or set value hover menu header(three dots) to show menu where you can choose desired action.</source>
+        <translation>K vložení výrazu, ze kterého byla proměnná spočítána, musíme kliknout na výraz proměnné v panelu proměnných. Pokud klikneme na proměnnou v panelu, tak vložíme její identifikátor a pravým kliknutím její hodnotu. Abychom nastavili nebo vymazali proměnnou, najedeme na hlavičku menu(tři tečky), tím zobrazíme menu, kde vybereme danou akci. </translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Barbie Calculator</source>
+        <translation type="vanished">Barbie Calculator</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="47"/>
+        <location filename="../qml/main.qml" line="51"/>
+        <location filename="../qml/main.qml" line="68"/>
+        <source>Help</source>
+        <translation>Nápověda</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="51"/>
+        <location filename="../qml/main.qml" line="66"/>
+        <source>About</source>
+        <translation>O programu</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="269"/>
         <source>Error</source>
         <translation>Chyba</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="223"/>
+        <location filename="../qml/main.qml" line="284"/>
         <source>Info</source>
         <translation>Info</translation>
     </message>
