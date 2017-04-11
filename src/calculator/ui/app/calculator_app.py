1c89abe0db6742fe5b9d13edb7ea9baa28ce3104
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2017-04-12 01:26:22 +0200

    Add colored print of qt console

--- src/calculator/ui/app/calculator_app.py
+++ src/calculator/ui/app/calculator_app.py
@@ -2,6 +2,8 @@
 import platform
 from typing import List
 
+from termcolor import colored
+
 from PyQt5.QtCore import (QSize, QtFatalMsg, QtCriticalMsg, QtWarningMsg, QtInfoMsg,
                           qInstallMessageHandler, QtDebugMsg)
 from PyQt5.QtCore import QTranslator
@@ -39,14 +41,22 @@ def qt_message_handler(mode, context, message):
         QtFatalMsg: "Fatal",
         QtDebugMsg: "Debug"
     }
-    mode = modes[mode]
+
+    modes_colors = {
+        QtInfoMsg: "blue",
+        QtWarningMsg: "yellow",
+        QtCriticalMsg: "red",
+        QtFatalMsg: "red",
+        QtDebugMsg: "green"
+    }
+
+    mode = colored(modes[mode], modes_colors[mode])
 
     if context.file is None:
         print('{mode}: {msg}'.format(mode=mode, msg=message))
     else:
-        print('{mode}: line: {line}, function: {func}, file: {file}'.format(
-            mode=mode, line=context.line, func=context.function, file=context.file))
-        print('{msg}'.format(msg=message))
+        print('{mode}: {msg}\t\t\t\tline: {line}, function: {func}, file: {file}'.format(
+            mode=mode, line=context.line, func=context.function, file=context.file, msg=message))
 
 qInstallMessageHandler(qt_message_handler)
 
