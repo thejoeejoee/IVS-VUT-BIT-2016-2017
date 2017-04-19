187f5345096c60716e10bfe96b83d5ca1b8f5192
Josef Kolář <thejoeejoee@gmail.com>
2017-04-19 14:23:14 +0200

    Bump version to RC1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ base_path = abspath(dirname(__file__))
 def setup():
     core.setup(
         name='calculator',
-        version='0.3',
+        version='1.0rc1',
         license='GNU GENERAL PUBLIC LICENSE Version 3',
         long_description=open(join(base_path, 'README.md')).read(),
         url='https://github.com/thejoeejoee/IVS-VUT-BIT-2016-2017',
