9350d608930d9b5696a8f8d4887083736e20a6e1
Martin Omacht <enkeymc@gmail.com>
2017-04-21 10:45:57 +0200

    Comment generator script

---
+++ dev/commentgen.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+authors_prefix='__author__ = '
+copyright='__copyright__ = "Copyright 2017, /dej/uran/dom team"'
+credits='__credits__ = ["Josef Kolář", "Son Hai Nguyen", "Martin Omacht", "Robert Navrátil"]'
+license='__license__ = "GPL-3.0"'
+
+writefiles=false
+
+if [ "$1" = "-w" ]; then
+	writefiles=true
+else
+	echo "Test run, rerun with -w argument to write comments to files."
+fi
+
+files=`find .. -type f -name '*.py'`
+#files="../src/tests/calculator/utils/__init__.py"
+
+echo "$files" | while read line; do
+	authors=`git blame $line | sed -r 's/^[^\(]+ \(//g' | sed -r 's/ [0-9]{4}.*$//g' | sed -r 's/ +$//g' | sort | uniq`
+	authors=`printf "$authors" | sed 's/EnkeyMC/Martin Omacht/g' | sed 's/Sony/Son Hai Nguyen/g' | sed 's/Meigurad/Robert Navrátil/g' | sort | uniq`
+	authors=`printf "$authors" | tr '\n' ',' | sed 's/,/, /g'`
+
+	linenum=`cat "$line" | nl -b a | grep -E ".*import.*$" | tail -n 1 | awk '{print $1}'`
+
+	if [ -z "$linenum" ]; then
+		content=`sed "1 a$license" "$line" | sed "1 a$credits" | sed "1 a$copyright" | sed "1 a$authors_prefix$authors" | sed "1 a\ "`
+		if $writefiles; then
+			printf "$content" > "$line"
+		fi
+		echo "In file '$line' inserted on line: 1"
+	else
+		content=`sed "$linenum a$license" "$line" | sed "$linenum a$credits" | sed "$linenum a$copyright" | sed "$linenum a$authors_prefix$authors" | sed "$linenum a\ "`
+		if $writefiles; then
+			printf "$content" > "$line"
+		fi
+		echo "In file '$line' inserted on line: $linenum"
+	fi
+
+done
