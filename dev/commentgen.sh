f2516b67895ade15339be824338cc918b9af886d
Martin Omacht <enkeymc@gmail.com>
2017-04-21 11:13:28 +0200

    Comment generator license change

--- dev/commentgen.sh
+++ dev/commentgen.sh
@@ -3,7 +3,7 @@
 authors_prefix='__author__ = '
 copyright='__copyright__ = "Copyright 2017, /dej/uran/dom team"'
 credits='__credits__ = ["Josef Kolář", "Son Hai Nguyen", "Martin Omacht", "Robert Navrátil"]'
-license='__license__ = "GPL-3.0"'
+license='__license__ = "GNU GPL Version 3"'
 
 writefiles=false
 
@@ -13,8 +13,7 @@ else
 	echo "Test run, rerun with -w argument to write comments to files."
 fi
 
-files=`find .. -type f -name '*.py'`
-#files="../src/tests/calculator/utils/__init__.py"
+files=`find .. -type f -name '*.py'
 
 echo "$files" | while read line; do
 	authors=`git blame $line | sed -r 's/^[^\(]+ \(//g' | sed -r 's/ [0-9]{4}.*$//g' | sed -r 's/ +$//g' | sort | uniq`
