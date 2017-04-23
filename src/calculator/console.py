b8de62d73ec486b0f61e8ba25c72293b8010e271
Martin Omacht <enkeymc@gmail.com>
2017-04-23 13:28:09 +0100

    Console interface improvements
    Added %print command to print variables
    No output on variable assign

--- src/calculator/console.py
+++ src/calculator/console.py
@@ -3,7 +3,7 @@ from pprint import pformat
 
 from calculator.core.calculator import Calculator
 
-__author__ = "Josef Kolář"
+__author__ = "Josef Kolář, Martin Omacht"
 __copyright__ = "Copyright 2017, /dej/uran/dom team"
 __credits__ = ["Josef Kolář", "Son Hai Nguyen", "Martin Omacht", "Robert Navrátil"]
 __license__ = "GNU GPL Version 3"
@@ -24,12 +24,33 @@ def main():
                 continue
             try:
                 for user_input in user_inputs:
-                    result, variables = calculator.process(user_input.strip())
-                    print(" === {}\n === {}\n".format(result, pformat(dict(variables))))
+                    user_input = user_input.strip()
+
+                    if user_input.startswith("%print"):
+                        _, variables = calculator.process(" ")
+                        user_input = user_input.split();
+                        if len(user_input) == 2:
+                            # print specific variable
+                            if user_input[1] in variables:
+                                value, src_expr, _ = variables[user_input[1]]
+                                print(" {:>4} === {}  ({})\n".format(user_input[1], value, src_expr))
+                            else:
+                                print("Variable '{}' does not exist.\n".format(user_input[1]))
+                        elif len(user_input) == 1:
+                            # print all variables
+                            for key, value in variables.items():
+                                print(" {:>4} === {}  ({})".format(key, value[0], value[1]))
+                            print("")
+                        else:
+                            print("Error: Unexpected number of parameters {}, expected 0 or 1.\n".format(len(user_input) - 1))
+                    else:
+                        result, _ = calculator.process(user_input)
+                        if result is not None:
+                            print("=== {}\n".format(result))
             except Exception as e:
-                print(e, repr(e))
+                print(e)
     except (SystemExit, KeyboardInterrupt, EOFError) as e:
-        pass
+        print("")
 
 
 if __name__ == '__main__':
